b8c2084c008b0fa690c670d0aed0a1577a9e3d3c
Ulises Rojas González <ulises.rogo@gmail.com>
2021-03-24 10:01:42 -0600

    Add interfaces to the project

---
+++ angular-project/src/app/interfaces/team.model.ts
@@ -0,0 +1,9 @@
+import { Countries } from './countries.enum';
+import { Player } from './player.model';
+
+export interface Team {
+  $key?: string;
+  name: string;
+  country: Countries;
+  players: Player[];
+}
