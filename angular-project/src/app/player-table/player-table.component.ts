9838bc434cd7f79a0b548521734cce739d2e569c
Ulises Rojas González <ulises.rogo@gmail.com>
2021-04-03 13:56:08 -0600

    Implement player deletion

--- angular-project/src/app/player-table/player-table.component.ts
+++ angular-project/src/app/player-table/player-table.component.ts
@@ -1,19 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
+import { take } from 'rxjs/operators';
 import { Player } from '../interfaces/player.model';
 import { PlayerService } from '../services/player.service';
+import { TeamService } from '../services/team.service';
 
 @Component({
   selector: 'app-player-table',
   templateUrl: './player-table.component.html',
-  styleUrls: ['./player-table.component.scss']
+  styleUrls: ['./player-table.component.scss'],
 })
 export class PlayerTableComponent implements OnInit {
   public players$ = new Observable<Player[]>();
   public selectedPlayer!: Player | null;
   public showModal = false;
 
-  constructor(private playerService: PlayerService) { }
+  constructor(
+    private playerService: PlayerService,
+    private teamService: TeamService
+  ) {}
 
   ngOnInit(): void {
     this.players$ = this.playerService.getPlayers();
@@ -28,11 +33,28 @@ export class PlayerTableComponent implements OnInit {
   }
 
   editPlayer(player: Player): void {
-    this.selectedPlayer = {...player};
+    this.selectedPlayer = { ...player };
     this.showModal = true;
     setTimeout(() => {
       window.location.replace('#open-modal');
     }, 0);
+  }
+
+  deletePlayer(player: Player): void {
+    this.teamService
+      .getTeams()
+      .pipe(take(1))
+      .subscribe((teams) => {
+        const modifiedPlayers = teams[0].players
+          ? teams[0].players.filter((p: any) => p.key !== player.$key)
+          : teams[0].players;
+        const formattedTeam = {
+          ...teams[0],
+          players: [...(modifiedPlayers ? modifiedPlayers : [])],
+        };
+        this.playerService.deletePlayer(player.$key as string);
+        this.teamService.editTeam(formattedTeam);
+      });
   }
 
   closeDialog(): void {
